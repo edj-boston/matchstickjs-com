b9fe2afad695bff63018f28781751db82b4cb54b
Eric Johnson <edj.boston@gmail.com>
2014-05-01 22:16:36 -0700

    Adding nice styles

--- js/custom.js
+++ js/custom.js
@@ -7,4 +7,14 @@ $(document).ready(function() {
 		return false;
 	});
 
+	// 
+	var h1 = $('h1').get(0);
+	var header = $(h1).html().substring(0, 10);
+	var badge = $(h1).html().substring(11, h1.length);
+
+	$(h1).html('<span class="match">' + header.substring(0, 5) + '</span>'
+		+ '<img id="matchstick" src="/img/matchstick.png" width="80">'
+		+ '<span class="stick">' + header.substring(5, 10) + '</span>');
+	//alert(badge);
+
 });
