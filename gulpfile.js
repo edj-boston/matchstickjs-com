667aba4b293a263faba2d8bc56e0e1e55857eaee
Eric Johnson <edj.boston@gmail.com>
2014-05-30 22:32:19 -0700

    First pass at gulp build file

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,10 @@
 var gulp = require('gulp');
 var uglify = require('gulp-uglify');
 var concat = require('gulp-concat');
-var rename = require("gulp-rename");
+var rename = require('gulp-rename');
+var clean = require('gulp-clean');
 
-// Minify and copy all JavaScript
+// Minify and combine all JavaScript
 gulp.task('scripts', function() {
 	gulp.src('js/custom.js')
 		.pipe(rename('custom.min.js'))
@@ -16,12 +17,44 @@ gulp.task('scripts', function() {
 		'js/jquery-1.11.0.min.js',
 		'js/custom.min.js'
 	]).pipe(concat('all.min.js'))
-    	.pipe(gulp.dest('js'))
+    	.pipe(gulp.dest('build'))
+});
+
+// Minify and combine all CSS
+gulp.task('css', function() {
+	gulp.src([
+		'css/bootstrap.min.css',
+		'css/custom.css',
+		'css/font-awesome.min.css'
+	]).pipe(concat('all.min.css'))
+    	.pipe(gulp.dest('build'))
+});
+
+// Minify and combine all CSS
+gulp.task('static', function() {
+	gulp.src('fonts/*')
+		.pipe(gulp.dest('build/fonts'));
+	gulp.src('inc/*')
+		.pipe(gulp.dest('build/inc'));
+	gulp.src('img/*')
+		.pipe(gulp.dest('build/img'));
+	gulp.src([
+		'index.html',
+		'favicon.ico',
+		'robots.txt'
+	])
+		.pipe(gulp.dest('build'));
 });
 
 // Default task
 gulp.task('default', function() {
+	gulp.src('build/*', {
+		read : false
+	}).pipe(clean());
+
 	gulp.run('scripts');
+	gulp.run('css');
+	gulp.run('static');
 
 	// Watch files and run tasks if they change
 	gulp.watch('js/*', function(event) {
