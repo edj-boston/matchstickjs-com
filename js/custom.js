bb2478e2a6589f220c70c5a10c9952823bc71a79
Eric Johnson <edj.boston@gmail.com>
2014-05-30 22:23:56 -0700

    Adding comments and GA

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,4 @@
+// jQuery behaviors
 $(document).ready(function() {
 
 	// Open external links in new window
@@ -15,6 +16,13 @@ $(document).ready(function() {
 	$(h1).html('<span class="match">' + header.substring(0, 5) + '</span>'
 		+ '<img id="matchstick" src="/img/matchstick.png" width="80">'
 		+ '<span class="stick">' + header.substring(5, 10) + '</span>');
-	//alert(badge);
 
 });
+
+// Google analytics
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+ga('create', 'UA-16075338-8', 'matchstickjs.com');
+ga('send', 'pageview');
