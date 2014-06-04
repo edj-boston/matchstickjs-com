2f53d3f97e6ce9ac726fc1c7d6d4e07eab46ea36
Eric Johnson <edj.boston@gmail.com>
2014-06-04 01:00:39 -0700

    Deleting old shell S3 deploy scripts

--- deploy-live.sh
+++
@@ -1,1 +0,0 @@
-aws s3 sync . s3://matchstickjs-com --exclude "*" --include "*.html" --include "*.css" --include "*.js" --include "*.png" --include "*.eot" --include "*.ttf" --include "*.woff" --include "*.svg" --include "*.ico" --include "*.txt" --delete
