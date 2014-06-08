fb05db2338e1284ab7b27118154560e30a1d3f0e
Eric Johnson <edj.boston@gmail.com>
2014-06-08 15:21:35 -0700

    Tests for dynamically created CSS

---
+++ test/css.js
@@ -0,0 +1,37 @@
+// External dependencies
+var assert = require('assert');
+var should = require('should');
+var fs = require('fs');
+
+
+// Test to see if dynamically created CSS is well-formed
+describe('The dynamically concatenated and minified CSS...', function() {
+
+	it('Should exist', function() {
+		if( !fs.existsSync('build/css/all.min.css') ) {
+			throw Error('/css/all.min.css does not exist');
+		}
+	});
+
+	it('Should contain Bootstrap styles', function() {
+		var file = fs.readFileSync('build/css/all.min.css', 'utf-8');
+		if(file.indexOf('Bootstrap') < 0) {
+			throw Error('/css/all.min.css does not contain Bootstrap styles');
+		}
+	});
+
+	it('Should contain Custom styles', function() {
+		var file = fs.readFileSync('build/css/all.min.css', 'utf-8');
+		if(file.indexOf('Custom Styles') < 0) {
+			throw Error('/css/all.min.css does not contain custom styles');
+		}
+	});
+
+	it('Should contain Font Awesome styles', function() {
+		var file = fs.readFileSync('build/css/all.min.css', 'utf-8');
+		if(file.indexOf('Font Awesome') < 0) {
+			throw Error('/css/all.min.css does not contain Font Awesome styles');
+		}
+	});
+
+});
