3800c546c939d22d7025902293c399cb5056cc50
Eric Johnson <edj.boston@gmail.com>
2014-06-08 11:54:22 -0700

    Tests for bower files

---
+++ test/bower.js
@@ -0,0 +1,29 @@
+// External dependencies
+var assert = require('assert');
+var should = require('should');
+var fs = require('fs');
+
+
+// Test to see if files copied from assets/bower made it okay
+describe('The assets copied from the assets/bower directory...', function() {
+
+	/* *
+	 * Fonts
+	 */
+
+	it('Should include Fontawesome files', function() {
+		if( !fs.existsSync('build/fonts/fontawesome-webfont.eot') ) {
+			throw Error('/fonts/fontawesome-webfont.eot does not exist');
+		}
+		if( !fs.existsSync('build/fonts/fontawesome-webfont.svg') ) {
+			throw Error('/fonts/fontawesome-webfont.svg does not exist');
+		}
+		if( !fs.existsSync('build/fonts/fontawesome-webfont.ttf') ) {
+			throw Error('/fonts/fontawesome-webfont.ttf does not exist');
+		}
+		if( !fs.existsSync('build/fonts/fontawesome-webfont.woff') ) {
+			throw Error('/fonts/fontawesome-webfont.woff does not exist');
+		}
+	});
+
+});
