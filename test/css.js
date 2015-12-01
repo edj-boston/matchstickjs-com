1168807e6df110729cd58b64cf6850a09a8d1812
Eric Johnson <edj.boston@gmail.com>
2015-11-30 21:57:39 -0800

    Updating tests to gunzip built files prior to introspection

--- test/css.js
+++ test/css.js
@@ -1,34 +1,34 @@
 // External dependencies
 var assert = require('assert'),
 	should = require('should'),
-	fs     = require('fs');
-
+	fs     = require('fs'),
+    zlib = require('zlib');
 
 // Test to see if dynamically created CSS is well-formed
 describe('The dynamically concatenated and minified CSS...', function() {
 
+ 	var handle = 'build/css/all.min.css';
+	var file = zlib.gunzipSync(fs.readFileSync(handle));
+
 	it('Should exist', function() {
-		if ( !fs.existsSync('build/css/all.min.css') ) {
+		if ( !fs.existsSync(handle) ) {
 			throw Error('/css/all.min.css does not exist');
 		}
 	});
 
 	it('Should contain Bootstrap styles', function() {
-		var file = fs.readFileSync('build/css/all.min.css', 'utf-8');
 		if (file.indexOf('Bootstrap') < 0) {
 			throw Error('/css/all.min.css does not contain Bootstrap styles');
 		}
 	});
 
 	it('Should contain Custom styles', function() {
-		var file = fs.readFileSync('build/css/all.min.css', 'utf-8');
 		if (file.indexOf('Custom Styles') < 0) {
 			throw Error('/css/all.min.css does not contain custom styles');
 		}
 	});
 
 	it('Should contain Font Awesome styles', function() {
-		var file = fs.readFileSync('build/css/all.min.css', 'utf-8');
 		if (file.indexOf('Font Awesome') < 0) {
 			throw Error('/css/all.min.css does not contain Font Awesome styles');
 		}
--- test/views.js
+++ test/views.js
@@ -3,41 +3,41 @@ var assert = require('assert'),
 	$	   = require('jquery')(require('jsdom').jsdom().defaultView),
 	should = require('should'),
 	fs     = require('fs'),
-	moment = require('moment');
+	moment = require('moment'),
+	zlib   = require('zlib');
 
 
 // Test to see if dynamically created HTML index is well-formed
 describe('The dynamically generated HTML index file...', function() {
 
+ 	var handle = 'build/index.html';
+	var file = zlib.gunzipSync(fs.readFileSync(handle));
+
 	it('Should exist', function() {
-		if ( !fs.existsSync('build/index.html') ) {
+		if ( !fs.existsSync(handle) ) {
 			throw Error('/index.html does not exist');
 		}
 	});
 
 	it('Should contain a <title> element from the header partial', function() {
-		var file = fs.readFileSync('build/index.html', 'utf-8');
 		if (file.indexOf('<title>MatchstickJS</title>') < 0) {
 			throw Error('/index.html does not contain the right <title> element');
 		}
 	});
 
 	it('Should contain an <h1> element from the markdown source', function() {
-		var file = fs.readFileSync('build/index.html', 'utf-8');
 		if ( $(file).find('h1') == 1 ) {
 			throw Error('/index.html does not contain an <h1> element');
 		}
 	});
 
 	it('Should contain an <ul> element from the share partial', function() {
-		var file = fs.readFileSync('build/index.html', 'utf-8');
 		if (file.indexOf('<ul id="share">') < 0) {
 			throw Error('/index.html does not contain the right <ul> element');
 		}
 	});
 
 	it('Should contain copyright text from the footer partial', function() {
-		var file = fs.readFileSync('build/index.html', 'utf-8');
 		var str = '&copy; ' + moment().format('YYYY');
 		if (file.indexOf(str) < 0) {
 			throw Error('/index.html does not contain the right copyright text');
@@ -49,28 +49,28 @@ describe('The dynamically generated HTML index file...', function() {
 // Test to see if dynamically created HTML error page is well-formed
 describe('The dynamically generated HTML error file...', function() {
 
+ 	var handle = 'build/404.html';
+	var file = zlib.gunzipSync(fs.readFileSync(handle));
+
 	it('Should exist', function() {
-		if ( !fs.existsSync('build/404.html') ) {
+		if ( !fs.existsSync(handle) ) {
 			throw Error('/404.html does not exist');
 		}
 	});
 
 	it('Should contain a <title> element from the header partial', function() {
-		var file = fs.readFileSync('build/404.html', 'utf-8');
 		if (file.indexOf('<title>MatchstickJS</title>') < 0) {
 			throw Error('/404.html does not contain the right <title> element');
 		}
 	});
 
 	it('Should contain an <h1> element with certain text', function() {
-		var file = fs.readFileSync('build/404.html', 'utf-8');
 		if (file.indexOf('<h1 id="page-not-found">Page not found</h1>') < 0) {
 			throw Error('/404.html does not contain the right <h1> element');
 		}
 	});
 
 	it('Should contain copyright text from the footer partial', function() {
-		var file = fs.readFileSync('build/404.html', 'utf-8');
 		var str = '&copy; ' + moment().format('YYYY');
 		if (file.indexOf(str) < 0) {
 			throw Error('/404.html does not contain the right copyright text');
