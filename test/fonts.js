b0f741cb050b163321e293269d37afc9ef6ea3b0
Eric Johnson <edj.boston@gmail.com>
2015-12-04 14:00:16 -0800

    Async font tests

--- test/fonts.js
+++ test/fonts.js
@@ -6,64 +6,44 @@ var assert = require('assert'),
 
 describe('The build/fonts directory...', function() {
 
-    it('Should include Font Awesome files', function() {
-        if ( !fs.existsSync('build/fonts/fontawesome-webfont.eot') ) {
-            throw Error('/fonts/fontawesome-webfont.eot does not exist');
-        }
-        if ( !fs.existsSync('build/fonts/fontawesome-webfont.svg') ) {
-            throw Error('/fonts/fontawesome-webfont.svg does not exist');
-        }
-        if ( !fs.existsSync('build/fonts/fontawesome-webfont.ttf') ) {
-            throw Error('/fonts/fontawesome-webfont.ttf does not exist');
-        }
-        if ( !fs.existsSync('build/fonts/fontawesome-webfont.woff') ) {
-            throw Error('/fonts/fontawesome-webfont.woff does not exist');
-        }
+    it('Should include the right font files', function(done) {
+        fs.readdir('build/fonts/', function(err, files) {
+            files.should.containEql('fontawesome-webfont.eot');
+            files.should.containEql('fontawesome-webfont.svg');
+            files.should.containEql('fontawesome-webfont.ttf');
+            files.should.containEql('fontawesome-webfont.woff');
+            done();
+        });
     });
 
-    it('Should include Merriweather font files', function() {
-        if ( !fs.existsSync('build/fonts/Merriweather.eot') ) {
-            throw Error('/fonts/Merriweather.eot does not exist');
-        }
-        if ( !fs.existsSync('build/fonts/Merriweather.svg') ) {
-            throw Error('/fonts/Merriweather.svg does not exist');
-        }
-        if ( !fs.existsSync('build/fonts/Merriweather.ttf') ) {
-            throw Error('/fonts/Merriweather.ttf does not exist');
-        }
-        if ( !fs.existsSync('build/fonts/Merriweather.woff') ) {
-            throw Error('/fonts/Merriweather.woff does not exist');
-        }
+    it('Should include Merriweather font files', function(done) {
+        fs.readdir('build/fonts/', function(err, files) {
+            files.should.containEql('Merriweather.eot');
+            files.should.containEql('Merriweather.svg');
+            files.should.containEql('Merriweather.ttf');
+            files.should.containEql('Merriweather.woff');
+            done();
+        });
     });
 
-    it('Should include Open-Sans font files', function() {
-        if ( !fs.existsSync('build/fonts/Open-Sans.eot') ) {
-            throw Error('/fonts/Open-Sans.eot does not exist');
-        }
-        if ( !fs.existsSync('build/fonts/Open-Sans.svg') ) {
-            throw Error('/fonts/Open-Sans.svg does not exist');
-        }
-        if ( !fs.existsSync('build/fonts/Open-Sans.ttf') ) {
-            throw Error('/fonts/Open-Sans.ttf does not exist');
-        }
-        if ( !fs.existsSync('build/fonts/Open-Sans.woff') ) {
-            throw Error('/fonts/Open-Sans.woff does not exist');
-        }
+    it('Should include Open-Sans font files', function(done) {
+        fs.readdir('build/fonts/', function(err, files) {
+            files.should.containEql('Open-Sans.eot');
+            files.should.containEql('Open-Sans.svg');
+            files.should.containEql('Open-Sans.ttf');
+            files.should.containEql('Open-Sans.woff');
+            done();
+        });
     });
 
-    it('Should include SourceCodePro font files', function() {
-        if ( !fs.existsSync('build/fonts/SourceCodePro.eot') ) {
-            throw Error('/fonts/SourceCodePro.eot does not exist');
-        }
-        if ( !fs.existsSync('build/fonts/SourceCodePro.svg') ) {
-            throw Error('/fonts/SourceCodePro.svg does not exist');
-        }
-        if ( !fs.existsSync('build/fonts/SourceCodePro.ttf') ) {
-            throw Error('/fonts/SourceCodePro.ttf does not exist');
-        }
-        if ( !fs.existsSync('build/fonts/SourceCodePro.woff') ) {
-            throw Error('/fonts/SourceCodePro.woff does not exist');
-        }
+    it('Should include SourceCodePro font files', function(done) {
+        fs.readdir('build/fonts/', function(err, files) {
+            files.should.containEql('SourceCodePro.eot');
+            files.should.containEql('SourceCodePro.svg');
+            files.should.containEql('SourceCodePro.ttf');
+            files.should.containEql('SourceCodePro.woff');
+            done();
+        });
     });
 
 });
