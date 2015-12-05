855a278bd15f7d0611cf465adacc757141a4f006
Eric Johnson <edj.boston@gmail.com>
2015-12-04 18:04:13 -0800

    Better assertions and more DRY

--- test/fonts.js
+++ test/fonts.js
@@ -5,44 +5,42 @@ var should = require('should'),
 
 describe('The build/fonts directory...', function() {
 
-    it('Should include the right font files', function(done) {
-        fs.readdir('build/fonts/', function(err, files) {
-            files.should.containEql('fontawesome-webfont.eot');
-            files.should.containEql('fontawesome-webfont.svg');
-            files.should.containEql('fontawesome-webfont.ttf');
-            files.should.containEql('fontawesome-webfont.woff');
+    var files = [];
+
+    before(function(done) {
+        fs.readdir('build/fonts/', function(err, arr) {
+            if (err) throw err;
+            files = arr;
             done();
         });
     });
 
-    it('Should include Merriweather font files', function(done) {
-        fs.readdir('build/fonts/', function(err, files) {
-            files.should.containEql('Merriweather.eot');
-            files.should.containEql('Merriweather.svg');
-            files.should.containEql('Merriweather.ttf');
-            files.should.containEql('Merriweather.woff');
-            done();
-        });
+    it('Should include the right font files', function() {
+        files.should.containEql('fontawesome-webfont.eot');
+        files.should.containEql('fontawesome-webfont.svg');
+        files.should.containEql('fontawesome-webfont.ttf');
+        files.should.containEql('fontawesome-webfont.woff');
     });
 
-    it('Should include Open-Sans font files', function(done) {
-        fs.readdir('build/fonts/', function(err, files) {
-            files.should.containEql('Open-Sans.eot');
-            files.should.containEql('Open-Sans.svg');
-            files.should.containEql('Open-Sans.ttf');
-            files.should.containEql('Open-Sans.woff');
-            done();
-        });
+    it('Should include Merriweather font files', function() {
+        files.should.containEql('Merriweather.eot');
+        files.should.containEql('Merriweather.svg');
+        files.should.containEql('Merriweather.ttf');
+        files.should.containEql('Merriweather.woff');
     });
 
-    it('Should include SourceCodePro font files', function(done) {
-        fs.readdir('build/fonts/', function(err, files) {
-            files.should.containEql('SourceCodePro.eot');
-            files.should.containEql('SourceCodePro.svg');
-            files.should.containEql('SourceCodePro.ttf');
-            files.should.containEql('SourceCodePro.woff');
-            done();
-        });
+    it('Should include Open-Sans font files', function() {
+        files.should.containEql('Open-Sans.eot');
+        files.should.containEql('Open-Sans.svg');
+        files.should.containEql('Open-Sans.ttf');
+        files.should.containEql('Open-Sans.woff');
+    });
+
+    it('Should include SourceCodePro font files', function() {
+        files.should.containEql('SourceCodePro.eot');
+        files.should.containEql('SourceCodePro.svg');
+        files.should.containEql('SourceCodePro.ttf');
+        files.should.containEql('SourceCodePro.woff');
     });
 
 });
